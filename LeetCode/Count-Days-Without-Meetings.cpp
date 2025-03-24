f0b0c978f25c141244d860ca1e3d5e8786c37df2
ronakgadhiya09 <120314724+ronakgadhiya09@users.noreply.github.com>
2025-03-24 16:00:46 +0530

    [3169] [Time Beats: 91.16%] [Memory Beats: 94.73%] - LeetPush

--- LeetCode/Count-Days-Without-Meetings.cpp
+++ LeetCode/Count-Days-Without-Meetings.cpp
@@ -1,30 +1,24 @@
 class Solution {
 public:
-    static bool comp(const vector<int> a,const vector<int> b){
+    static bool comp(const vector<int>& a, const vector<int>& b) {
         return a[0] < b[0];
     }
 
     int countDays(int days, vector<vector<int>>& meetings) {
-
-        sort(meetings.begin(),meetings.end(),comp);
+        sort(meetings.begin(), meetings.end(), comp);
 
         int prevEnd = 0;
-        int prevStart = 0;
-        for(auto &meeting : meetings){
+        for (auto& meeting : meetings) {
             int start = meeting[0];
             int end = meeting[1];
 
-            if(end <= prevEnd) continue;
-            else if(start <= prevEnd){
-                int interval = end - prevEnd;
-                days -= interval;
-            }
-            else{
+            if (start > prevEnd) {
                 days -= (end - start + 1);
+                prevEnd = end;
+            } else if (end > prevEnd) {
+                days -= (end - prevEnd);
+                prevEnd = end;
             }
-
-            prevEnd = max(end,prevEnd);
-            prevStart = start;
         }
 
         return days;
