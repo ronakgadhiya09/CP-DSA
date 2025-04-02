6b18bca019a1c68637facb0a13600cf6d0635c1b
ronakgadhiya09 <120314724+ronakgadhiya09@users.noreply.github.com>
2025-04-02 12:28:23 +0530

    [2873] [Time Beats: 31.33%] [Memory Beats: 35.84%] - LeetPush

---
+++ LeetCode/Maximum-Value-of-an-Ordered-Triplet-I.cpp
@@ -0,0 +1,19 @@
+typedef long long ll;
+class Solution {
+public:
+    long long maximumTripletValue(vector<int>& nums) {
+
+        int n = nums.size();
+
+        long long val = 0;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = i + 1 ; j < n ; j++){
+                for(int k = j + 1 ; k < n ; k++){
+                    val = max(val,ll(((ll)nums[i] - (ll)nums[j]) * (ll)nums[k]));
+                }
+            }
+        }
+
+        return val;
+    }
+};
