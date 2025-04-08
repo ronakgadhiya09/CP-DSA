d3b35bbedba0e81263651f04cd17a55bced15cb8
ronakgadhiya09 <120314724+ronakgadhiya09@users.noreply.github.com>
2025-04-08 23:38:33 +0530

    [3396] [Time Beats: 100.00%] [Memory Beats: 98.46%] - LeetPush

---
+++ LeetCode/Minimum-Number-of-Operations-to-Make-Elements-in-Array-Distinct.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+
+        int n = nums.size();
+        vector<bool> seen(101,false);
+
+        for(int i = n - 1 ; i >= 0 ; i--){
+            if(seen[nums[i]]) return i/3 + 1;
+            seen[nums[i]] = true;
+        }
+
+        return 0;
+    }
+};
