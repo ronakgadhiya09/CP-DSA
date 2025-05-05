9ed2de0851e02c2edfe6ca561b32b8344026b7ee
ronakgadhiya09 <120314724+ronakgadhiya09@users.noreply.github.com>
2025-05-05 17:10:22 +0530

    [790] [Time Beats: 100.00%] [Memory Beats: 45.99%] - LeetPush

---
+++ LeetCode/Domino-and-Tromino-Tiling.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    const int mod = 1e9 + 7;
+public:
+    int numTilings(int n) {
+        vector<int> dp(n+1,0);
+
+        if(n<3) return n;
+
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 5;
+
+        for(int i = 4 ; i <= n ;i++) dp[i] = ((2*dp[i-1])%mod + dp[i-3])%mod;
+
+        return dp[n];
+    }
+};
