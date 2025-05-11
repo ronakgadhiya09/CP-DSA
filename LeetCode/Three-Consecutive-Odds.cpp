4ed3f88e358e42832d00c7eaf016332f1c696f1c
ronakgadhiya09 <120314724+ronakgadhiya09@users.noreply.github.com>
2025-05-11 10:55:09 +0530

    [1550] [Time Beats: 100.00%] [Memory Beats: 98.85%] - LeetPush

---
+++ LeetCode/Three-Consecutive-Odds.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int n = arr.size();
+        if(n < 3) return false;
+        for(int i = 1 ; i < n - 1 ;i++){
+            if((arr[i-1]&1) & (arr[i]&1) & (arr[i+1]&1)) return true;
+        }
+        return false;
+    }
+};
