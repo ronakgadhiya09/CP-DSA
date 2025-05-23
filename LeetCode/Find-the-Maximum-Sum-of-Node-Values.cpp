76f69d88337725396b248818e2f801411407d7af
ronakgadhiya09 <120314724+ronakgadhiya09@users.noreply.github.com>
2025-05-23 09:46:35 +0530

    [3068] [Time Beats: 20.64%] [Memory Beats: 18.71%] - LeetPush

--- LeetCode/Find-the-Maximum-Sum-of-Node-Values.cpp
+++ LeetCode/Find-the-Maximum-Sum-of-Node-Values.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
-    int n, k;
-    static constexpr int MAXN = 20000;
-    long long dp[MAXN][2];
-
-    long long dfs(int index, bool xorApplied, const vector<int>& nums) {
-        if (index == n)
+    long long dfs(int index, bool xorApplied, const vector<int>& nums, int k, 
+                                                    vector<vector<long long>>& dp) {
+        if (index == nums.size())
             return xorApplied ? LLONG_MIN : 0;
 
         if (dp[index][xorApplied] != -1)
             return dp[index][xorApplied];
 
-        long long normal = nums[index] + dfs(index + 1, xorApplied, nums);
-        long long withXor = (nums[index] ^ k) + dfs(index + 1, !xorApplied, nums);
+        long long normal = nums[index] + dfs(index + 1, xorApplied, nums, k, dp);
+        long long withXor = (nums[index] ^ k) + dfs(index + 1, !xorApplied, nums, k, dp);
 
         return dp[index][xorApplied] = max(normal, withXor);
     }
 
     long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) {
-        this->n = nums.size();
-        this->k = k;
-
-        memset(dp, -1, sizeof(dp));
-
-        return dfs(0, false, nums);
+        int n = nums.size();
+        vector<vector<long long>> dp(n, vector<long long>(2, -1));
+        return dfs(0, false, nums, k, dp);
     }
 };
