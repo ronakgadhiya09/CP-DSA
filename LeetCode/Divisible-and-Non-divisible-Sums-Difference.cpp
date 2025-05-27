fd43aa426206c2a75f174b19aa010ebccac34d3f
ronakgadhiya09 <120314724+ronakgadhiya09@users.noreply.github.com>
2025-05-28 00:04:18 +0530

    [2894] [Time Beats: 100.00%] [Memory Beats: 36.04%] - LeetPush

--- LeetCode/Divisible-and-Non-divisible-Sums-Difference.cpp
+++ LeetCode/Divisible-and-Non-divisible-Sums-Difference.cpp
@@ -1,10 +1,7 @@
 class Solution {
 public:
     int differenceOfSums(int n, int m) {
-        int not_divisible = 0;
-        for(int i = m ; i <= n ;i+=m){
-            not_divisible += i;
-        }
-        return n*(n+1)/2 - 2*not_divisible;
+        int k = n/m;
+        return n*(n+1)/2 - k*(k+1)*m;
     }
 };
