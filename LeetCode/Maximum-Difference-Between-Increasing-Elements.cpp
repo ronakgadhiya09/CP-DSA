313808eb45e4cb8274f89d079b552fab940606c5
ronakgadhiya09 <120314724+ronakgadhiya09@users.noreply.github.com>
2025-06-16 19:42:40 +0530

    [2016] [Time Beats: 100.00%] [Memory Beats: 5.67%] - LeetPush

--- LeetCode/Maximum-Difference-Between-Increasing-Elements.cpp
+++ LeetCode/Maximum-Difference-Between-Increasing-Elements.cpp
@@ -3,12 +3,11 @@ public:
     int maximumDifference(vector<int>& nums) {
         int max_dif = -1;
         int n = nums.size();
-        for(int i = 0 ; i < n ; i++){
-            for(int j = i + 1 ; j < n ;j++){
-                if(nums[j] > nums[i])
-                    max_dif = max(max_dif,nums[j] - nums[i]);
-            }
+        vector<int> suf_max(n+1,-1);
+        for(int i = n - 1 ; i >= 0 ; i--){
+            suf_max[i] = max(suf_max[i+1],nums[i]);
         }
+        for(int i = 0 ; i < n ;i++) max_dif = max(max_dif,suf_max[i] > nums[i] ? suf_max[i] - nums[i] : -1);
         return max_dif;
     }
 };
