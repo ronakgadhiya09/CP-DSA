23a9294dcb34a5d3cb4a4401e16604c7df10d88a
ronakgadhiya09 <120314724+ronakgadhiya09@users.noreply.github.com>
2025-06-27 00:02:04 +0530

    [2311] [Time Beats: 100.00%] [Memory Beats: 50.79%] - LeetPush

--- LeetCode/Longest-Binary-Subsequence-Less-Than-or-Equal-to-K.cpp
+++ LeetCode/Longest-Binary-Subsequence-Less-Than-or-Equal-to-K.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     int longestSubsequence(string s, int k) {
-        int sm = 0;
-        int cnt = 0;
-        int bits = 32 - __builtin_clz(k);
+        int count = 0;
+        int value = 0;
         int n = s.size();
+        int bits = 0;
 
-        for (int i = 0; i < n; i++){
-            char ch = s[n - i - 1];
-            if (ch == '1'){
-                if (i < bits && sm + (1 << i) <= k){
-                    sm += 1 << i;
-                    cnt++;
+        for (int i = 0; i < n; i++) {
+            char ch = s[n - 1 - i];
+            if (ch == '0') {
+                count++;
+            }else {
+                if (i < 31 && value + (1 << i) <= k) {
+                    value += 1 << i;
+                    count++;
                 }
-            } 
-            else{
-                cnt++;
             }
         }
-
-        return cnt;
+        return count;
     }
 };
