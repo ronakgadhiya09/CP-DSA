0d5b4336e7e3c1c38a468c15f418d88c50483f33
ronakgadhiya09 <120314724+ronakgadhiya09@users.noreply.github.com>
2025-07-09 23:30:53 +0530

    [3439] [Time Beats: 100.00%] [Memory Beats: 59.76%] - LeetPush

---
+++ LeetCode/Reschedule-Meetings-for-Maximum-Free-Time-I.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxFreeTime(int eventTime, int k, vector<int>& startTime, vector<int>& endTime) {
+        int n = startTime.size();
+        vector<int> gaps(n + 1);
+        gaps[0] = (int)startTime[0] - 0;
+
+        for (int i = 1; i < n; ++i) {
+            gaps[i] = (int)startTime[i] - endTime[i - 1];
+        }
+
+        gaps[n] = (int)eventTime - endTime[n - 1];
+
+        vector<int> prefix(n + 2, 0);
+        for (int i = 1; i <= n + 1; ++i) {
+            prefix[i] = prefix[i - 1] + gaps[i - 1];
+        }
+
+        int best = 0;
+        for (int i = k + 1; i <= n + 1; ++i) {
+            int cur = prefix[i] - prefix[i - (k + 1)];
+            best = max(best, cur);
+        }
+
+        return best;
+    }
+};
