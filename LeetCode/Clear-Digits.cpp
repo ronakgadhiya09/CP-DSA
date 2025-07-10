f5cfe09ae67a7e1fcadab59be892e1b5be372b7f
ronakgadhiya09 <120314724+ronakgadhiya09@users.noreply.github.com>
2025-07-10 11:20:07 +0530

    [3174] [Time Beats: 100.00%] [Memory Beats: 66.15%] - LeetPush

---
+++ LeetCode/Clear-Digits.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        int charIndex = 0;
+
+        while (charIndex < s.size()) {
+            if (isdigit(s[charIndex])) {
+                s.erase(charIndex, 1);
+                if (charIndex > 0) {
+                    s.erase(charIndex - 1, 1);
+                    charIndex--;
+                }
+            }
+            else{
+                charIndex++;
+            }
+        }
+        return s;
+    }
+};
