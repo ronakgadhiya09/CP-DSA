1b5dde45ba789bc826da8eb908a62469a81732bb
ronakgadhiya09 <120314724+ronakgadhiya09@users.noreply.github.com>
2025-08-09 18:13:42 +0530

    [808] [Time Beats: 52.02%] [Memory Beats: 49.66%] - LeetPush

--- LeetCode/Soup-Servings.cpp
+++ LeetCode/Soup-Servings.cpp
@@ -3,9 +3,9 @@ private:
     unordered_map<int, unordered_map<int, double>> dp;
 
     double calcProb(int a, int b) {
-        if (a <= 0 && b <= 0) return 0.5; // both empty same time
-        if (a <= 0) return 1.0;           // A empty first
-        if (b <= 0) return 0.0;           // B empty first
+        if (a <= 0 && b <= 0) return 0.5; 
+        if (a <= 0) return 1.0;           
+        if (b <= 0) return 0.0;
 
         if (dp[a][b]) return dp[a][b];
 
@@ -21,8 +21,8 @@ private:
 
 public:
     double soupServings(int n) {
-        if (n >= 4800) return 1.0; 
         int m = (n + 24) / 25;
+        if(m>=300) return 1.0;
         return calcProb(m, m);
     }
 };
