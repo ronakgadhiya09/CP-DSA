5e35b546e2aa64f7e94fff21bf792f83b5d123b6
ronakgadhiya09 <120314724+ronakgadhiya09@users.noreply.github.com>
2025-08-13 15:21:37 +0530

    [30] [Time Beats: 71.74%] [Memory Beats: 66.09%] - LeetPush

--- LeetCode/Substring-with-Concatenation-of-All-Words.cpp
+++ LeetCode/Substring-with-Concatenation-of-All-Words.cpp
@@ -11,15 +11,17 @@ public:
         for(const string &w : words) need[w]++;
 
         vector<int> ans;
+        // process m possible offsets
         for(int offset = 0; offset < m; ++offset){
             int left = offset;
-            int count = 0;
+            int count = 0; // number of words currently in window
             unordered_map<string,int> window;
             for(int right = offset; right + m <= k; right += m){
                 string w = s.substr(right, m);
                 if(need.count(w)){
                     window[w]++;
                     count++;
+                    // if this word occurs too many times, move left forward
                     while(window[w] > need[w]){
                         string leftWord = s.substr(left, m);
                         window[leftWord]--;
@@ -28,6 +30,7 @@ public:
                     }
                     if(count == n) {
                         ans.push_back(left);
+                        // move left one word to continue searching for next possible
                         string leftWord = s.substr(left, m);
                         window[leftWord]--;
                         left += m;
@@ -35,6 +38,7 @@ public:
                     }
                 } 
                 else {
+                    // invalid word — reset window
                     window.clear();
                     count = 0;
                     left = right + m;
