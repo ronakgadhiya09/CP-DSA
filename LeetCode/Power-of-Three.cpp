0696a12cd7c2d4bf593378986186dd2089f902a9
ronakgadhiya09 <120314724+ronakgadhiya09@users.noreply.github.com>
2025-08-13 12:35:22 +0530

    [326] [Time Beats: 47.44%] [Memory Beats: 76.82%] - LeetPush

--- LeetCode/Power-of-Three.cpp
+++ LeetCode/Power-of-Three.cpp
@@ -2,8 +2,7 @@ class Solution {
     const int x = 1162261467; // 3^19 -> largest pow of 3 < 2^31
 public:
     bool isPowerOfThree(int n) {
-        if(n<=0) return false;
-        if(x%n) return false;
+        if(n<=0 || x%n) return false;
         return true;
     }
 };
