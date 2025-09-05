5e598376b44364565c970e88a4a4ace310fdee40
ronakgadhiya09 <120314724+ronakgadhiya09@users.noreply.github.com>
2025-09-05 17:32:49 +0530

    [2749] [Time Beats: 100.00%] [Memory Beats: 52.07%] - LeetPush

---
+++ LeetCode/Minimum-Operations-to-Make-the-Integer-Zero.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int makeTheIntegerZero(int num1, int num2) {
+        if (num1 == 0) return 0;
+        for (int k = 1; k <= 60; ++k) {
+            long long S = (long long)num1 - (long long)k * num2;
+            if (S < k) continue;
+            if (__builtin_popcountll(S) <= k && k <= S) return k;
+        }
+        return -1;
+    }
+};
