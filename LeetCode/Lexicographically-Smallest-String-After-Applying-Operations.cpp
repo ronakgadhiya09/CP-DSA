4930ca917d14b9dad6500aba499e571cccb61afb
ronakgadhiya09 <120314724+ronakgadhiya09@users.noreply.github.com>
2025-10-19 13:33:29 +0530

    [1625] [Time Beats: 18.14%] [Memory Beats: 54.87%] - LeetPush

---
+++ LeetCode/Lexicographically-Smallest-String-After-Applying-Operations.cpp
@@ -0,0 +1,27 @@
+class Solution {
+private:
+    void findNum(string &s,int a,int b,set<string> &seen){
+        int n = s.size();
+        string t = s;
+        for(int i = 1; i <n; i+=2) t[i] = ((t[i] - '0' + a) % 10 ) + '0';
+        if(!seen.count(t)){
+            seen.insert(t);
+            findNum(t,a,b,seen);
+        }
+
+        t = s.substr(n-b,b) + s.substr(0,n-b);
+        if(!seen.count(t)){
+            seen.insert(t);
+            findNum(t,a,b,seen);
+        }
+
+        return;
+    }
+
+public:
+    string findLexSmallestString(string s, int a, int b) {
+        set<string> seen;
+        findNum(s,a,b,seen);
+        return *seen.begin();
+    }
+};
